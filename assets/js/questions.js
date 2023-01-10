e8bf30dfbb87ad3c6b7e2001b25a87d341712055
knoxjess <110634800+knoxjess@users.noreply.github.com>
2023-01-09 23:09:35 -0600

    changed font and finshed css stlye

---
+++ assets/js/questions.js
@@ -0,0 +1,50 @@
+// list of all questions, choices, and answers
+var questions = [
+    {
+      title: 'Which of the following tags do not require a terminator?',
+      choices: ['<u>', '<br>', '<b>', 'none of the above'],
+      answer: '<br>',
+    },
+    {
+      title: 'The condition in an if / else statement is enclosed within ____.',
+      choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+      answer: 'parentheses',
+    },
+    {
+      title: 'What is CSS?',
+    choices: ['CSS is a style sheet language', 'CSS is designed to separate the presentation and content, including layout, colors, and fonts', 'CSS is the language used to style the HTML documents', 'All of the mentioned'],
+    answer: 'All of the mentioned',
+    },
+    {
+      title: 'Arrays in JavaScript can be used to store ____.',
+      choices: [
+        'numbers and strings',
+        'other arrays',
+        'booleans',
+        'all of the above',
+      ],
+      answer: 'all of the above',
+    },
+    {
+      title: 'Which of the following tag is used to embed css in html page?',
+      choices: ['<css>' ,'<!DOCTYPE html>', '<script>','<style>'],
+      answer: '<style>',
+
+    },
+    {
+      title:'String values must be enclosed within ____ when being assigned to variables.',
+      choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+      answer: 'quotes',
+    },
+    {
+      title: ' Which of the following CSS selectors are used to specify a group of elements?',
+      choices: ['tag','id','class','both'],
+      answer: 'class',
+    },
+    {
+      title:'A very useful tool used during development and debugging for printing content to the debugger is:',
+      choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
+      answer: 'console.log',
+    },
+  ];
+  
